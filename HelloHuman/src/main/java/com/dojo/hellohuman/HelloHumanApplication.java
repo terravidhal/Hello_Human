2df32e2105d2b99a8220ae087ae2f9d52471f413
terravidhal <infiniterraetincelle@gmail.com>
2023-10-25 06:14:51 +0100

    hellohuman

---
+++ HelloHuman/src/main/java/com/dojo/hellohuman/HelloHumanApplication.java
@@ -0,0 +1,14 @@
+package com.dojo.hellohuman;
+
+import org.springframework.boot.SpringApplication;
+//add
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloHumanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHumanApplication.class, args);
+	}
+
+}
